937e4bab66041d0a36aa771b107cce96d60b9157
suhyun <suhyuuuuuun@gmail.com>
2022-11-01 17:24:01 +0900

    20221101 | up

--- oracle004_subquery.sql
+++ oracle004_subquery.sql
@@ -136,7 +136,97 @@ WHERE department_id IN (
 SELECT department_id, department_name
 FROM departments d
 WHERE EXISTS (
-                SELECT 1 -- 임의의 값을 넣어줌
+                SELECT 1 -- 임의의 값을 넣어줌 ex) 1, 'x' -> 존재론적으로 따지는 것이기 때문에 SELECT값은 의미가 없음
+                FROM employees e
+                WHERE e.department_id = d.department_id -- 메인쿼리에 있는 department_id를 사용 -> 상관관계
+              );
+              
+-- 사원이 없는 부서만 출력하시오
+SELECT department_id, department_name
+FROM departments d
+WHERE NOT EXISTS (
+                SELECT 1 
                 FROM employees e
                 WHERE e.department_id = d.department_id
               );
+              
+-- 부서가 있는 사원의 정보를 출력하시오
+SELECT e.employee_id, e.first_name, e.department_id
+FROM employees e
+WHERE EXISTS (
+                SELECT 1
+                FROM departments d
+                WHERE d.department_id = e.department_id
+              );
+              
+-- 부서가 없는 사원의 정보를 출력하시오
+SELECT e.employee_id, e.first_name, e.department_id
+FROM employees e
+WHERE NOT EXISTS (
+                SELECT 1
+                FROM departments d
+                WHERE d.department_id = e.department_id
+              );
+              
+-- 관리자가 있는 사원의 정보를 출력하시오
+SELECT count(*)
+FROM employees
+WHERE manager_id IS NOT NULL; -- 106
+
+SELECT w.employee_id, w.first_name, w.manager_id
+FROM employees w
+WHERE EXISTS (
+                SELECT 1
+                FROM employees m
+                WHERE m.employee_id = w.manager_id
+                );
+
+-- 관리자가 없는 사원의 정보를 출력하시오                
+SELECT w.employee_id, w.first_name, w.manager_id
+FROM employees w
+WHERE NOT EXISTS (
+                SELECT 1
+                FROM employees m
+                WHERE m.employee_id = w.manager_id
+                );
+                
+/*==============================================================================
+Top-N 서브쿼리
+    상위의 값을 추출할 때 사용한다.
+    <, <= 연산자를 사용할 수 있다. 단 비교되는 값이 1일때는 =도 가능하다.
+    ORDER BY절을 사용할 수 있다.
+==============================================================================*/
+
+-- 급여가 가장 높은 상위 3명을 검색하시오
+SELECT rownum, emp.first_name, emp.salary
+FROM(
+        SELECT first_name, salary
+        FROM employees
+        ORDER BY salary DESC
+    ) emp -- emp : 가상의 테이블명
+WHERE rownum <=3; /*rownum은 select의 출력순서이다.*/
+
+-- 급여가 가장 높은 상위 4위부터 8위까지를 검색하시오.
+-- ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+SELECT emp2.*
+FROM(
+        SELECT rownum AS rm, emp.*
+        FROM(
+                SELECT first_name, salary
+                FROM employees
+                ORDER BY salary DESC
+                )emp
+    )emp2
+WHERE emp2.rm >=4 AND emp2.rm <=8;
+
+-- 월 별 입사자 수를 조회하되 입사자 수가 가장 많은 상위 3개만 출력되도록 하시오
+-- <출력:  월 입사자 수 >
+
+SELECT * 
+FROM(
+        SELECT to_char(hire_date, 'mm') AS 월, count(*) AS 입사자수
+        FROM employees
+        GROUP BY to_char(hire_date, 'mm')
+        ORDER BY 입사자수 DESC
+    )month
+WHERE rownum <=3;
