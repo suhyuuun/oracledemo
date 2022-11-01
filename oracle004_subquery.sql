0f8df7367b7e9eb9491cfe9f5b3a6fcb3bfe1e34
suhyun <suhyuuuuuun@gmail.com>
2022-11-01 15:46:54 +0900

    20221101 | up

--- oracle004_subquery.sql
+++ oracle004_subquery.sql
@@ -63,4 +63,80 @@ WHERE e.department_id = d.department_id
 AND salary > (
                 SELECT salary
                 FROM employees
-                WHERE first_name = 'Bruce');
+                WHERE first_name = 'Bruce')
+ORDER BY e.salary;
+
+-- Steven와 같은 부서에서 근무하는 사원의 이름, 급여, 입사일을 출력하시오(in)
+SELECT e.first_name, e.salary, e.hire_date
+FROM employees e
+
+WHERE department_id IN(
+                         SELECT department_id
+                         FROM employees
+                         WHERE first_name = 'Steven'
+                       );
+
+-- 부서별로 가장 급여를 많이 받는 사원이름, 부서번호, 급여를 출력하시오 (in)
+SELECT e.first_name, e.department_id, e.salary
+FROM employees e
+WHERE (e.department_id, e.salary) IN ( --정렬이 두개이면, in안에서 나오는 값도 두개
+                                       SELECT department_id, max(salary)
+                                       FROM employees
+                                       GROUP BY department_id -- where절에서 order by 사용불가 / from 절에서는 order by 사용가능
+                                       );
+ORDER BY e.department_id;
+
+-- 30부서에 소속된 모든 사원들의 급여보다 더 많은 급여를 받는 사원이름, 급여, 입사일을 출력하시오(ALL)
+-- 서브쿼리에서 max()함수를 사용하지 않는다.
+SELECT salary
+FROM employees
+WHERE department_id=30;
+
+SELECT first_name, salary, hire_date
+FROM employees
+WHERE salary >ALL( 
+                    SELECT salary
+                    FROM employees
+                    WHERE department_id=30 -- 최대 : 11000
+                    );
+                 
+-- 30부서 사원들이 받은 최저급여보다 높은 급여를 받는 사원이름, 급여, 입사일을 출력하시오(ANY)
+-- 서브쿼리에서 min()함수를 사용하지 않는다.
+SELECT first_name, salary, hire_date
+FROM employees
+WHERE salary > ANY( 
+                    SELECT salary
+                    FROM employees
+                    WHERE department_id=30 -- 최저 : 2500
+                    )
+ORDER BY salary;
+
+-- 20부서 사원이 있으면 사람들의 사원명, 입사일, 급여, 부서번호를 출력하시오
+SELECT first_name, hire_date, salary, department_id
+FROM employees
+WHERE EXISTS ( -- 존재하면 true (값을 가지고 옴) 없으면 false (값을 가지고 오지 않음)
+                SELECT department_id
+                FROM employees
+                WHERE department_id = 20
+                ); -- 상관관계 쿼리 처리할 때 사용 /상관관계쿼리 :  메인쿼리에서 사용하는 컬럼을 서브쿼리에서 참조하는 것
+                
+-- 사원이 있는 부서만 출력하시오
+SELECT count(*)
+FROM departments; /*총부서 27개*/
+
+SELECT department_id, department_name
+FROM departments
+WHERE department_id IN (
+                        SELECT distinct department_id
+                        FROM employees
+                        WHERE department_id IS NOT NULL
+                        --GROUP BY department_id --중복제거용
+                         );
+
+SELECT department_id, department_name
+FROM departments d
+WHERE EXISTS (
+                SELECT 1 -- 임의의 값을 넣어줌
+                FROM employees e
+                WHERE e.department_id = d.department_id
+              );
