ad4fa1371cb6617a487b01ddde63ebe96006bf88
suhyun <suhyuuuuuun@gmail.com>
2022-11-02 16:24:40 +0900

    20221102 | up

--- oracle007_ddl_constraint.sql
+++ oracle007_ddl_constraint.sql
@@ -150,3 +150,189 @@ ROLLBACK;
 -- ORA-00942: table or view does not exist
 SELECT * FROM members;
 
+/*==============================================================================
+무결성 제약조건
+    무결성이 데이터베이스 내에 있는 데이터의 정확성 유지를 의미한다면 
+    제약조건은 바람직하지않는 데이터가 저장되는 것을 방지하는 것을 말한다.
+    
+무결성 제약조건 5종류 : not null, unique, primary key, foreign key, check ★★★★★
+    1. not null : null을 허용하지 않는다
+    2. unique : 중복된 값을 허용하지 않는다. 항상 유일한 값이다.
+    3. primary key : not null + unique
+    4. foreign key : 참조되는 테이블의 컬럼의 값이 존재하면 허용된다.
+    5. check : 저장가능한 데이터의 값의 범위나 조건을 지정하여 설정한 값만을 허용한다.
+    
+무결성 제약조건 2가지 레벨 : 컬럼레벨, 테이블 레벨
+
+    컬럼레벨 설정
+    CREATE TABLE emp06(
+        id varchar2(10) constraint emp06_id_pk primary key,
+        name varchar2(20) constraint emp06_name_nn not null, 
+        age number(3) constraint emp06_age_ck check(age between 20 and 50),
+        gen char(1) constraint emp06_gen_ck check(gen in('m','w'))   
+     );
+     
+     테이블 레벨 설정
+     CREATE TABLE emp07(
+        id varchar2(10),
+        name varchar2(20) constraint emp07_name_nn not null, 
+        age number(3), 
+        gen char(1),   
+        constraint emp07_id_pk primary key(id),
+        constraint emp07_ck check(age between 20 and 50),
+        constraint emp07_gen_ck check(gen in('m','w'))
+     );
+     
+==============================================================================*/
+
+SELECT * FROM user_constraints; -- 데이터 딕셔너리에 저장되어있음
+
+CREATE TABLE emp06(
+        id varchar2(10) constraint emp06_id_pk primary key,
+        name varchar2(20) constraint emp06_name_nn not null, 
+        age number(3) constraint emp06_age_ck check(age between 20 and 50),
+        gen char(1) constraint emp06_gen_ck check(gen in('m','w'))   
+     );
+     
+SELECT * FROM user_constraints
+WHERE constraint_name LIKE '%EMP06%';
+
+-- ORA-02290: check constraint (HR.EMP06_AGE_CK) violated
+INSERT INTO emp06(id, name, age, gen)
+VALUES ('kim','김고수',15,'m');
+
+-- ORA-02290: check constraint (HR.EMP06_GEN_CK) violated
+INSERT INTO emp06(id, name, age, gen)
+VALUES ('kim','김고수',25,'p');
+
+-- 정상 삽입
+INSERT INTO emp06(id, name, age, gen)
+VALUES ('kim','김고수',25,'m');
+
+-- ORA-00001: unique constraint (HR.EMP06_ID_PK) violated
+INSERT INTO emp06(id, name, age, gen)
+VALUES ('kim','김고수',25,'m');
+
+-- ORA-01400: cannot insert NULL into ("HR"."EMP06"."ID")
+INSERT INTO emp06(id, name, age, gen)
+VALUES (null,'김고수',25,'m');
+
+
+
+-- 테이블 제거
+DROP TABLE emp06;
+
+-- 테이블 제거되었는지 확인하는 과정
+SELECT * FROM user_constraints
+WHERE constraint_name LIKE '%EMP06%';
+
+-- primary key 와 foreign key
+
+CREATE TABLE dept01(
+    deptno number(2) constraint dept01_deptno_pk primary key,
+    dname varchar2(20)
+    );
+
+INSERT INTO dept01(deptno, dname)
+VALUES(10, 'accounting');
+
+SELECT * FROM dept01;
+
+INSERT INTO dept01(deptno, dname)
+VALUES(20, 'sales');
+
+SELECT * FROM dept01;
+
+INSERT INTO dept01(deptno, dname)
+VALUES(30, 'research');
+
+SELECT * FROM dept01;
+
+CREATE TABLE loc01(
+    locno number(2),
+    locname varchar2(20),
+    constraint loc01_locno_pk primary key(locno)
+);
+
+INSERT INTO loc01(locno, locname)
+VALUES(11, 'seoul');
+
+SELECT * FROM loc01;
+
+INSERT INTO loc01(locno, locname)
+VALUES(12, 'jeju');
+
+SELECT * FROM loc01;
+
+INSERT INTO loc01(locno, locname)
+VALUES(13,'busan');
+
+SELECT * FROM loc01;
+
+CREATE TABLE emp08(
+    empno number(2) constraint emp08_empno_pk primary key,
+    deptno number(2) constraint emp08_deptno_fk references dept01(deptno),
+    locno number(2),
+    constraint emp08_locno_fk foreign key(locno) references loc01(locno)
+);
+
+SELECT * FROM emp08;
+
+-- 정상 삽입
+INSERT INTO emp08(empno, deptno, locno)
+VALUES(1,10,11);
+
+SELECT * FROM emp08;
+
+INSERT INTO emp08(empno, deptno, locno)
+VALUES(2, 20,12);
+
+SELECT * FROM emp08;
+
+INSERT INTO emp08(empno, deptno, locno)
+VALUES(3, null,null);
+
+SELECT * FROM emp08;
+
+-- ORA-02291: integrity constraint (HR.EMP08_DEPTNO_FK) violated - parent key not found
+-- deptno에는 값이10, 20, 30만 있음
+INSERT INTO emp08(empno, deptno, locno)
+VALUES(4, 40, 11);
+
+-- ORA-02291: integrity constraint (HR.EMP08_LOCNO_FK) violated - parent key not found
+-- locno에는 값이 11,12,13만 있음
+INSERT INTO emp08(empno, deptno, locno)
+VALUES(5, 30, 14);
+
+-- emp08테이블의 deptno컬럼에서 30을 참조하지 않기 때문에 이상없이 삭제가능하다.
+DELETE FROM dept01
+WHERE deptno = 30;
+
+SELECT * FROM dept01;
+
+SELECT * FROM emp08;
+
+-- ORA-02292: integrity constraint (HR.EMP08_DEPTNO_FK) violated - child record found
+DELETE FROM dept01
+WHERE deptno = 20;
+
+/*
+다른 테이블에 현재 테이블을 참조해서 사용하고 있을 때는 제약조건을 제거한 후 현재 테이블의 데이터를 삭제한다.
+*/
+
+SELECT * FROM user_constraints
+WHERE constraint_name LIKE '%EMP08%';
+
+-- deptno = 20과 연결되어있는 컬럼 연결 끊기
+ALTER TABLE emp08
+DROP CONSTRAINT emp08_deptno_fk;
+
+-- 정상 삭제
+DELETE FROM dept01
+WHERE deptno=20;
+
+SELECT * FROM dept01;
+
+/*
+부모키가 
+*/
