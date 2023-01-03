d199209be1928990a0bcd0bae53cfe3eab54fabc
suhyun <suhyuuuuuun@gmail.com>
2023-01-04 04:03:01 +0900

    20230104 | update

---
+++ 제목 없음.sql
@@ -0,0 +1,13 @@
+drop table pmap;
+
+create table map(
+res_name varchar2(2000),
+p_num varchar2(2000),
+area varchar2(2000),
+address varchar2(2000),
+latitude varchar2(2000),
+longitude varchar2(2000));
+
+insert into jimatmap values('인더비엣','02-595-3824','서울',');
+insert into jimatmap values();
+commit;
