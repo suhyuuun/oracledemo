ee03318f7d7b3dc18f2fe0cf2196678a6997b0de
suhyun <suhyuuuuuun@gmail.com>
2023-02-23 08:35:31 +0900

    update

---
+++ ugotfilm_insert_test.sql
@@ -0,0 +1,7 @@
+SELECT * FROM ugotfilm_movie;
+SELECT * FROM ugotfilm_cast;
+SELECT * FROM ugotfilm_director;
+SELECT * FROM ugotfilm_movie_choice;
+SELECT * FROM ugotfilm_cast_choice;
+SELECT * FROM ugotfilm_director_choice;
+SELECT * FROM ugotfilm_genre_choice;
